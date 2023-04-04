68098091d9f4598cc52d8264ebfdacaa4c0318fa
Fiedor_ <stachfie@wp.pl>
2023-04-04 09:49:10 +0200

    Napisane i działające skrypty

--- samochody.php
+++ samochody.php
@@ -17,14 +17,32 @@
     <div id="left">
         <h2>DZIŚ POLECAMY TOYOTĘ ROCZNIK 2014</h2>
         <?php
+        $kw1 = mysqli_query($conn, "SELECT `id`,`model`,`kolor` FROM `samochody` WHERE `marka`='Toyota' AND `rocznik`='2014'");
+        foreach ($kw1 as $a) {
+            echo $a['id'] . " Toyota " . $a['model'] . " Kolor: " . $a['kolor'];
+        }
         ?>
         <h2>WSZYSTKIE DOSTĘPNE SAMOCHODY</h2>
         <?php
+        $kw2 = mysqli_query($conn, "SELECT `id`,`marka`,`model`,`rocznik` FROM `samochody`;");
+        foreach ($kw2 as $a) {
+            foreach ($a as $row) {
+                echo $row . "&nbsp";
+            }
+            echo "<br>";
+        }
         ?>
     </div>
     <div id="mid">
         <h2>ZAMÓWIONE AUTA NUMERAMI TELEFONU KLIENTÓW</h2>
         <?php
+        $kw3 = mysqli_query($conn, "SELECT `samochody`.`id`,`marka`,`model`,`telefon` FROM `samochody` INNER JOIN `zamowienia` ON `samochody`.`id`=`zamowienia`.`samochody_id`;");
+        foreach ($kw3 as $a) {
+            foreach ($a as $row) {
+                echo $row . "&nbsp";
+            }
+            echo "<br>";
+        }
         ?>
     </div>
     <div id="right">
